3d5840e8c8e6a547bf38112269cc7ec1d38f3a96
abhay963 <abhayyadav96312@gmail.com>
2025-06-30 22:47:11 +0530

    changed the port to 4000

--- Serverr/server.js
+++ Serverr/server.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 
 
 const app=express();
-const port=process.env.PORT||3000
+const port=process.env.PORT||4000
 
 app.use(express.json());
 app.use(cookieParser());
