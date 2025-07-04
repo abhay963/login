bcb51c0e6de041119eb0bab18221c3f1c9f6d5a8
abhay963 <abhayyadav96312@gmail.com>
2025-07-04 15:48:03 +0530

    userAuth updated

--- Serverr/middleware/userAuth.js
+++ Serverr/middleware/userAuth.js
@@ -1,25 +1,22 @@
 import jwt from "jsonwebtoken";
 
-const userAuth =async(requestAnimationFrame,resizeBy,next)=>{
-    const {token}=req.cookies;
+const userAuth = async (req, res, next) => {
+    const { token } = req.cookies;
 
-    if(!token){
-        return res.json({success:false,message:'Not Authorized. Login Again'})
+    if (!token) {
+        return res.status(401).json({ success: false, message: 'Not Authorized. Login Again' });
     }
     try {
-        
-        const tokenDecode=jwt.verify(token,process.env/JWT_SECRET);
-if(tokenDecode.id){
-    req.body.userId=tokenDecode.id;
-}
-else{
-    return res.json({succes:false,message:"Not authorized. Login Again"})
-}
-next();
-
+        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+        if (tokenDecode.id) {
+            req.userId = tokenDecode.id;
+            next();
+        } else {
+            return res.status(401).json({ success: false, message: "Not authorized. Login Again" });
+        }
     } catch (error) {
-        res.json({success:false,message:error.message});
+        return res.status(401).json({ success: false, message: error.message });
     }
-}
+};
 
 export default userAuth;
